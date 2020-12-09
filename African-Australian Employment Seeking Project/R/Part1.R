1a6cbfaa17421f852c888bdcc47a3a5c5d9bffba
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2020-12-09 22:51:43 +1030

    Part1 update

--- African-Australian Employment Seeking Project/R/Part1.R
+++ African-Australian Employment Seeking Project/R/Part1.R
@@ -19,30 +19,25 @@ data_d31_ref <- data %>%
   select("responseid", starts_with("d31_")) # will use this to check colnames changes 
 
 # renaming column names. Possible with rename_with() and regex?
-new_names <- c("responseid", "Job_opportunities_in_South_Australia", 
-               "The_strength_of_your_personal_social_networks",
-               "Your_knowledge_about_Australian_culture",
-               "Employers_knowledge_about_your_culture",
-               "Your_former_employment_experience",
-               "Your_level_of_qualification_being_too_high",
-               "Your_level_of_qualification_being_to_low",
-               "The_country_you_received_your_qualification_in", "Your_race","Your_religion",
-               "Your_ethnicity", "Your_gender", "Your_disability", "Your_language_or_accent",
-               "Employers_perceptions_about_Africans_in_Australia",
-               "Your_limited_prior_employment_interview_experience", "Your_age","Your_visa_status",
-               "Your_name", "Your_country_of_birth")
+new_names <- c("responseid", "Job opportunities in South Australia", 
+               "Strength of personal social networks",
+               "Knowledge about Australian culture",
+               "Employers knowledge about your culture",
+               "Previous employment experience",
+               "Level of qualification being too high",
+               "Level of qualification being too low",
+               "The country you received your qualification in", "Race","Religion",
+               "Ethnicity", "Gender", "Disability", "Language or accent",
+               "Employers perceptions about Africans in Australia",
+               "Limited prior employment interview experience", "Age","Visa status",
+               "Name", "Country of birth")
 
 colnames(data_d31) <- new_names # converts column names to those above
 #Check if one of the column is numeric
-is.numeric(data_d31$Your_country_of_birth) # returns TRUE (str(data_31) returns numeric for all columns)
+is.numeric(data_d31$`Job opportunities in South Australia`) # returns TRUE (str(data_31) returns numeric for all columns)
 # convert all columns, except first one, to factor
 data_d31 <- data_d31 %>%
   mutate_if(is.double, as.factor)
-
-glimpse(data_d31) # now all columns are factors
-
-# convert responseid column to double
-data_d31$responseid <- as.numeric(data_d31$responseid)
 
 glimpse(data_d31) # now all columns are factors
 
@@ -51,22 +46,22 @@ data_d31 %>%
   summarise_if(is.factor, nlevels)
 
 # get levels of one column
-levels(data_d31$Job_opportunities_in_South_Australia)
+levels(data_d31$`Job opportunities in South Australia`)
 
 # change factor levels by hand
 data_d31 <- data_d31 %>%
-  mutate_at( .vars = vars(Job_opportunities_in_South_Australia, 
-                          The_strength_of_your_personal_social_networks,
-                          Your_knowledge_about_Australian_culture,
-                          Employers_knowledge_about_your_culture,
-                          Your_former_employment_experience,
-                          Your_level_of_qualification_being_too_high,
-                          Your_level_of_qualification_being_to_low,
-                          The_country_you_received_your_qualification_in, Your_race,Your_religion,
-                          Your_ethnicity, Your_gender, Your_disability, Your_language_or_accent,
-                          Employers_perceptions_about_Africans_in_Australia,
-                          Your_limited_prior_employment_interview_experience, Your_age,Your_visa_status,
-                          Your_name, Your_country_of_birth),
+  mutate_at( .vars = vars(`Job opportunities in South Australia`, 
+                          `Strength of personal social networks`,
+                          `Knowledge about Australian culture`,
+                          `Employers knowledge about your culture`,
+                          `Previous employment experience`,
+                          `Level of qualification being too high`,
+                          `Level of qualification being too low`,
+                          `The country you received your qualification in`, `Race`,`Religion`,
+                          `Ethnicity`, `Gender`, `Disability`, `Language or accent`,
+                          `Employers perceptions about Africans in Australia`,
+                          `Limited prior employment interview experience`, `Age`,`Visa status`,
+                          `Name`, `Country of birth`),
              .funs = fct_recode, 
              "Not at all" = "1",
              "Slightly" = "2",
@@ -78,11 +73,14 @@ data_d31 <- data_d31 %>%
 # pivoting data into longer format
 
 data_d31 <- data_d31 %>%
+  select( `responseid`, `Strength of personal social networks`,
+         `Knowledge about Australian culture`, `Employers knowledge about your culture`,
+         `Race`, `Ethnicity`, `Language or accent`,
+         `Employers perceptions about Africans in Australia`, `Country of birth`) %>%
   pivot_longer(!responseid, names_to = "question", values_to = "response") %>%
-  select(!responseid) %>%   # remove the responseid column
   group_by(question, response) %>%
   count(name = "Total") %>%
-  filter(!is.na(response))   # remove rows with NA values in response column
+  filter(!is.na(response) & response != "No answer")   # remove rows with NA values in response column
 
 # Plotting
 ggplot(data = data_d31, aes(x = question, y = Total, fill = response)) +
@@ -95,20 +93,20 @@ data_d31_d4 <- data %>%
   select("responseid", starts_with(c("d31_", "d4")))
 
 # renaming column names. Possible with rename_with() and regex?
-new_names_3 <- c("responseid", "Job opportunities in South Australia", 
-               "The strength of your personal social networks",
-               "Your knowledge about Australian culture",
-               "Employers knowledge about your culture",
-               "Your former employment experience",
-               "Your level of qualification being too high",
-               "Your level of qualification being to low",
-               "The country you received your qualification in", "Your race","Your religion",
-               "Your ethnicity", "Your gender", "Your disability", "Your language or accent",
-               "Employers perceptions about Africans in Australia",
-               "Your limited prior employment interview experience", "Your age","Your visa status",
-               "Your name", "Your country of birth", "Country of birth")
+new_names_2 <- c("responseid", "Job opportunities in South Australia", 
+                 "Strength of personal social networks",
+                 "Knowledge about Australian culture",
+                 "Employers knowledge about your culture",
+                 "Previous employment experience",
+                 "Level of qualification being too high",
+                 "Level of qualification being too low",
+                 "The country you received your qualification in", "Race","Religion",
+                 "Ethnicity", "Gender", "Disability", "Language or accent",
+                 "Employers perceptions about Africans in Australia",
+                 "Limited prior employment interview experience", "Age","Visa status",
+                 "Name", "Your country of birth", "Country of birth")
 
-colnames(data_d31_d4) <- new_names_3 # converts column names to those above
+colnames(data_d31_d4) <- new_names_2 # converts column names to those above
 #Check if one of the column is numeric
 is.numeric(data_d31_d4$`Your country of birth`) # returns TRUE (str(data_31) returns numeric for all columns)
 # convert all columns, except first one, to factor
@@ -127,23 +125,21 @@ glimpse(data_d31_d4) # now all columns are factors
 data_d31_d4 %>%
   summarise_if(is.factor, nlevels)
 
-# get levels of one column
-levels(data_d31$Job_opportunities_in_South_Australia)
 
 # change factor levels by hand
 data_d31_d4 <- data_d31_d4 %>%
   mutate_at( .vars = vars(`Job opportunities in South Australia`, 
-                          `The strength of your personal social networks`,
-                          `Your knowledge about Australian culture`,
+                          `Strength of personal social networks`,
+                          `Knowledge about Australian culture`,
                           `Employers knowledge about your culture`,
-                          `Your former employment experience`,
-                          `Your level of qualification being too high`,
-                          `Your level of qualification being to low`,
-                          `The country you received your qualification in`, `Your race`,`Your religion`,
-                          `Your ethnicity`, `Your gender`, `Your disability`, `Your language or accent`,
+                          `Previous employment experience`,
+                          `Level of qualification being too high`,
+                          `Level of qualification being too low`,
+                          `The country you received your qualification in`, `Race`,`Religion`,
+                          `Ethnicity`, `Gender`, `Disability`, `Language or accent`,
                           `Employers perceptions about Africans in Australia`,
-                          `Your limited prior employment interview experience`, `Your age`,`Your visa status`,
-                          `Your name`, `Your country of birth`),
+                          `Limited prior employment interview experience`, `Age`,`Visa status`,
+                          `Name`, `Your country of birth`),
              .funs = fct_recode, 
              "Not at all" = "1",
              "Slightly" = "2",
@@ -155,11 +151,14 @@ data_d31_d4 <- data_d31_d4 %>%
 # pivoting data into longer format
 
 data_d31_d4 <- data_d31_d4 %>%
+  select(`responseid`, `Strength of personal social networks`,
+         `Knowledge about Australian culture`, `Employers knowledge about your culture`,
+         `Race`, `Ethnicity`, `Language or accent`,
+         `Employers perceptions about Africans in Australia`, `Your country of birth`, `Country of birth`) %>%
   pivot_longer(!c(responseid, `Country of birth`), names_to = "question", values_to = "response") %>%
-  select(!responseid) %>%   # remove the responseid column
   group_by(question, response, `Country of birth`) %>%
   count(name = "Total") %>%
-  filter(!is.na(response))   # remove rows with NA values in response column
+  filter(!is.na(response) & response != "No answer")   # remove rows with NA values in response column
 
 # Collapse country names
 data_d31_d4 <- data_d31_d4 %>%
@@ -169,7 +168,8 @@ data_d31_d4 <- data_d31_d4 %>%
                                                           "Congo",  "Congo, DR", "DEM. REP . CONGO", 
                                                           "DR Congo", "Congo dr", "D. R Congo", 
                                                           "Democratic republic of Congo", "DRC"),
-                                           `South Sudan` = c("South Sudan", "South sudan")))
+                                           `South Sudan` = c("South Sudan", "South sudan"),
+                                           `Ghana` = "ghana"))
 
 # Plotting
 ggplot(data = data_d31_d4, aes(x = question, y = Total, fill = response)) +
@@ -182,20 +182,20 @@ data_d31_s1 <- data %>%
   select("responseid", starts_with(c("d31_", "s1")))
 
 # renaming column names. Possible with rename_with() and regex?
-new_names_4 <- c("responseid", "Job opportunities in South Australia", 
-                 "The strength of your personal social networks",
-                 "Your knowledge about Australian culture",
+new_names_3 <- c("responseid", "Job opportunities in South Australia", 
+                 "Strength of personal social networks",
+                 "Knowledge about Australian culture",
                  "Employers knowledge about your culture",
-                 "Your former employment experience",
-                 "Your level of qualification being too high",
-                 "Your level of qualification being to low",
-                 "The country you received your qualification in", "Your race","Your religion",
-                 "Your ethnicity", "Your gender", "Your disability", "Your language or accent",
+                 "Previous employment experience",
+                 "Level of qualification being too high",
+                 "Level of qualification being too low",
+                 "The country you received your qualification in", "Race","Religion",
+                 "Ethnicity", "Gender", "Disability", "Language or accent",
                  "Employers perceptions about Africans in Australia",
-                 "Your limited prior employment interview experience", "Your age","Your visa status",
-                 "Your name", "Your country of birth", "Current work status")
+                 "Limited prior employment interview experience", "Age","Visa status",
+                 "Name", "Country of birth", "Current work status")
 
-colnames(data_d31_s1) <- new_names_4 # converts column names to those above
+colnames(data_d31_s1) <- new_names_3 # converts column names to those above
 #Check if one of the column is numeric
 is.numeric(data_d31_s1$responseid) # returns TRUE (str(data_31) returns numeric for all columns)
 # convert all columns, except first one, to factor
@@ -215,22 +215,22 @@ data_d31_s1 %>%
   summarise_if(is.factor, nlevels)
 
 # get levels of one column
-levels(data_d31_s1$`Job opportunities in South Australia`)
+#levels(data_d31_s1$`Job opportunities in South Australia`)
 
 # change factor levels by hand
 data_d31_s1 <- data_d31_s1 %>%
   mutate_at( .vars = vars(`Job opportunities in South Australia`, 
-                          `The strength of your personal social networks`,
-                          `Your knowledge about Australian culture`,
+                          `Strength of personal social networks`,
+                          `Knowledge about Australian culture`,
                           `Employers knowledge about your culture`,
-                          `Your former employment experience`,
-                          `Your level of qualification being too high`,
-                          `Your level of qualification being to low`,
-                          `The country you received your qualification in`, `Your race`,`Your religion`,
-                          `Your ethnicity`, `Your gender`, `Your disability`, `Your language or accent`,
+                          `Previous employment experience`,
+                          `Level of qualification being too high`,
+                          `Level of qualification being too low`,
+                          `The country you received your qualification in`, `Race`,`Religion`,
+                          `Ethnicity`, `Gender`, `Disability`, `Language or accent`,
                           `Employers perceptions about Africans in Australia`,
-                          `Your limited prior employment interview experience`, `Your age`,`Your visa status`,
-                          `Your name`, `Your country of birth`),
+                          `Limited prior employment interview experience`, `Age`,`Visa status`,
+                          `Name`, `Country of birth`),
              .funs = fct_recode, 
              "Not at all" = "1",
              "Slightly" = "2",
@@ -251,11 +251,16 @@ data_d31_s1 <- data_d31_s1 %>%
 
 # pivoting data into longer format
 data_d31_s1 <- data_d31_s1 %>%
+  select(`responseid`,`Strength of personal social networks`,
+         `Knowledge about Australian culture`, `Employers knowledge about your culture`,
+         `Race`, `Ethnicity`, `Language or accent`,
+         `Employers perceptions about Africans in Australia`, `Country of birth`,
+         `Current work status`) %>%
   pivot_longer(!c(responseid, `Current work status`), names_to = "question", values_to = "response") %>%
   select(!responseid) %>%   # remove the responseid column
   group_by(question, response, `Current work status`) %>%
   count(name = "Total") %>%
-  filter(!is.na(response))   # remove rows with NA values in response column
+  filter(!is.na(response) & response != "No answer")  # remove rows with NA values in response column
 
 
 # Plotting
