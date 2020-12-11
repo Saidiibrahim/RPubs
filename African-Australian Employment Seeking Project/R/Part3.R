ae2bd5bda0f720525ab022fb6ec3e87509943317
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2020-12-11 14:09:10 +1030

    update

--- African-Australian Employment Seeking Project/R/Part3.R
+++ African-Australian Employment Seeking Project/R/Part3.R
@@ -14,12 +14,12 @@ data <- read_xlsx("Dataset/RTIS_Migrant Study_11May.xlsx", sheet = "Complete") %
 data_d30 <- data %>%
   select("responseid", starts_with("d30_"))
 # renaming column names. Possible with rename_with() and regex?
-new_name_5 <- c("responseid", "The process was transparent", 
+new_name_6 <- c("responseid", "The process was transparent", 
                "The process was conducted efficiently",
                "The process was fair",
-               "I was provided adequate opportunity to seek feedback on my application")
+               "Adequate opportunity to seek feedback")
 
-colnames(data_d30) <- new_name_5
+colnames(data_d30) <- new_name_6
 
 # lets look at the data structure
 glimpse(data_d30)
@@ -40,7 +40,7 @@ data_d30 <- data_d30 %>%
   mutate_at(.vars = vars(`The process was transparent`,
                          `The process was conducted efficiently`,
                          `The process was fair`,
-                         `I was provided adequate opportunity to seek feedback on my application`),
+                         `Adequate opportunity to seek feedback`),
             .funs = fct_recode,
             "Strongly disagree" = "1",
             "Disagree" = "2",
@@ -51,16 +51,15 @@ data_d30 <- data_d30 %>%
 
 
 # pivoting data into longer format
-
 data_d30 <- data_d30 %>%
-  pivot_longer(!responseid, names_to = "question", values_to = "response") %>%
+  pivot_longer(!responseid, names_to = "Question", values_to = "Response") %>%
   select(!responseid) %>%   # remove the responseid column
-  group_by(question, response) %>%
+  group_by(Question, Response) %>%
   count(name = "Total") %>%
-  filter(!is.na(response))   # remove rows with NA values in response column
+  filter(!is.na(Response) & Response != "No answer")    # remove rows with NA values in response column
 
 # Plotting
-ggplot(data = data_d30, aes(x = question, y = Total, fill = response)) +
+ggplot(data = data_d30, aes(x = Question, y = Total, fill = Response)) +
   geom_bar(stat = "identity", position = "dodge") +
   coord_flip()
 
@@ -70,10 +69,10 @@ ggplot(data = data_d30, aes(x = question, y = Total, fill = response)) +
 data_d38_d20 <- data %>%
   select("responseid", starts_with(c("d38", "d20")))
 # renaming column names. Possible with rename_with() and regex?
-new_name_6 <- c("responseid", "Country of highest education", 
+new_name_7 <- c("responseid", "Country of highest education", 
                 "Current job", "Job2", "Job3", "Job4")
 
-colnames(data_d38_d20) <- new_name_6
+colnames(data_d38_d20) <- new_name_7
 
 # glimpse
 glimpse(data_d38_d20)
@@ -98,7 +97,8 @@ data_d38_d20 <- data_d38_d20 %>%
                                            `DR Congo` = c("Congo DRC", "DR Congo",
                                                           "Republic democratic of Congo",
                                                           "Democratic Republic of Congo",
-                                                          "DRC", "Dr congo")))
+                                                          "DRC", "Dr congo"),
+                                           `Ghana` = "ghana"))
 
 
 
@@ -109,38 +109,68 @@ data_d38_d20 <- data_d38_d20 %>%
             "No" = "2")
 # pivoting data into longer format
 data_d38_d20 <- data_d38_d20 %>%
-  pivot_longer(!c(responseid, `Country of highest education`), names_to = "question", values_to = "response") %>%
+  pivot_longer(!c(responseid, `Country of highest education`), names_to = "Question", values_to = "Response") %>%
   select(!responseid) %>%   # remove the responseid column
-  group_by(question, response, `Country of highest education`) %>%
+  group_by(Question, Response, `Country of highest education`) %>%
   count(name = "Total") %>%
-  filter(!is.na(response))   # remove rows with NA values in response column
+  filter(!is.na(Response) & Response != "No answer")    # remove rows with NA values in response column
 
 
 # Plotting
-ggplot(data = data_d38_d20, aes(x = question, y = Total, fill = response)) +
+ggplot(data = data_d38_d20, aes(x = Question, y = Total, fill = Response)) +
   geom_bar(stat = "identity", position = "dodge") +
-  #facet_wrap(~ `Country of highest education`) +
+  facet_wrap(~ `Country of highest education`) +
   coord_flip()
 
 # D20 are your qualifications required at each of your jobs and D24a_5 ----
-data_d20_d24a <- data %>%
-  select("responseid", starts_with(c("d20_", "d24a_")))
+data_d20_d24a5 <- data %>%
+  select("responseid", starts_with(c("d20_", "d24a_5")))
 # renaming column names. Possible with rename_with() and regex?
-#new_name_7 <- c("responseid", "The process was transparent", 
-#                "The process was conducted efficiently",
-#                "The process was fair",
-#                "I was provided adequate opportunity to seek feedback on my application")
+new_name_8 <- c("responseid", "Current job", "Job2", "Job3", 
+                "Job4", "Job satisfaction")
+colnames(data_d20_d24a5) <- new_name_8
+
+glimpse(data_d20_d24a5)
+
+data_d20_d24a5 <- data_d20_d24a5 %>%
+  mutate_if(is.double, as.factor)
+
+glimpse(data_d20_d24a5)
+
+# Recode factors
+data_d20_d24a5 <- data_d20_d24a5 %>%
+  mutate_at(.vars = vars(`Current job`, Job2, Job3, Job4),
+            .funs = fct_recode,
+            "Yes" = "1",
+            "No" = "2")
+
+# Recode factors in job satisfaction column
+
+data_d20_d24a5 <- data_d20_d24a5 %>%
+  mutate_at(.vars = vars(`Job satisfaction`),
+            .funs = fct_recode,
+            "Totally dissastisfied" = "1",
+            "Dissatisfied" = "2",
+            "Undecided" = "3",
+            "Satisfied" = "4",
+            "Totally satisfied" = "5",
+            "Refused" = "6",
+            "Don't know" = "7")
+
+# Transform to longer format
+data_d20_d24a5 <- data_d20_d24a5 %>%
+  pivot_longer(!c(responseid, `Job satisfaction`), names_to = "Question", values_to = "Response") %>%
+  select(!responseid) %>%   # remove the responseid column
+  group_by(Question, Response, `Job satisfaction`) %>%
+  count(name = "Total") %>%
+  filter(!is.na(Response) & Response != "Refused")  # remove rows with NA values in response column
 
 
-
-
-
-
-
-
-
-
-
+# Plotting
+ggplot(data = data_d20_d24a5, aes(x = Question, y = Total, fill = Response)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  facet_wrap(~ `Job satisfaction`) +
+  coord_flip()
 
 
 
